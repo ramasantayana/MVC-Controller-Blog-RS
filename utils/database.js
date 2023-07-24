a1ee16cf98b668a066d93afc324bd6b881cde945
ramasantayana <rama.santayana@uconn.edu>
2023-07-24 14:59:51 -0400

    added db conf

--- utils/database.js
+++ utils/database.js
@@ -1,10 +1,13 @@
 const mysql2=require('mysql2');
 const Sequelize =require('sequelize');
 require('dotenv').config();
-const sequelize = new Sequelize('blog', 'root', process.env.DB_PASS, {
-    host:'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host:process.env.DB_HOST,
     dialect: 'mysql',
     dialectModule:mysql2
   })
 
 module.exports=sequelize;
+
+
+
